88aae0da17ab8672878eb13dfb4ed31c334e54e6
Jonny Graybill <jon.a.graybill@gmail.com>
2020-05-11 21:50:53 -0700

    working server for test database

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Index file is the entry point for the server
+
+const express = require('express') // Module
+const bodyParser = require('body-parser') // Middleware
+const app = express() // Variable
+const port = 3000 // Variable
+const db = require('./queries')
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+// Tell a route to look for a GET request on the root (/) URL, and then return some JSON
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+// HTTP routes
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+// Set the app to listen on the set port
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
---
+++ queries.js
@@ -0,0 +1,86 @@
+'use strict';
+
+// Set up configuration of PostgreSQL connection
+
+// In production, these configuration details should go into a separate file with restrictive permissions not accessible from version control
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'me',
+  host: 'localhost',
+  database: 'api',
+  password: 'password',
+  port: 5432,
+})
+
+// Create route endpoints
+
+// GET all users
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+// GET a single user by id
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id)
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+// POST a new user
+const createUser = (request, response) => {
+  const { name, email } = request.body
+
+  pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${results.insertId}`)
+  })
+}
+
+// PUT updated data into an existing user
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email } = request.body
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+
+// DELETE a user
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
+// Export functions so they can be used in the index.js file
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+}
